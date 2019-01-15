a7da9b5f7ead10f951bd92a295aa42d28197960d
Андрей Скуднов <askudnov@ad.local>
2019-01-15 16:50:07 +0300

    last push

--- src/com/springinaction/springidol/MainClass.java
+++ src/com/springinaction/springidol/MainClass.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 public class MainClass {
   public static void main(String[] args) {
 		 ApplicationContext context = new FileSystemXmlApplicationContext("resources//spring-idol.xml");
-		Performer performer = (Performer) context.getBean("kenny");
+		Performer performer = (Performer) context.getBean("hank");
 		performer.perform();
 	}
 }
---
+++ src/com/springinaction/springidol/OneManBand.java
@@ -0,0 +1,18 @@
+package com.springinaction.springidol;
+import java.util.Map; 
+import com.springinaction.springidol.Instrument;
+import com.springinaction.springidol.Performer;
+public class OneManBand implements Performer {    
+public OneManBand() {}
+    public void perform() {    
+    for (String key : instruments.keySet()) {  
+	System.out.print(key + " : ");     
+	Instrument instrument = instruments.get(key); 
+	instrument.play();       
+	}  
+	}
+    private Map<String, Instrument> instruments;
+    public void setInstruments(Map<String, Instrument> instruments) {
+        this.instruments = instruments;  // Внедрение инструментов в виде  
+		}                               // отображения (Map)
+		}
