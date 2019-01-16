4f3b3f1bbde518bd41e17d823d7806cb34ad9da3
Андрей Скуднов <askudnov@ad.local>
2019-01-16 17:35:19 +0300

    next

--- spring.bat
+++ spring.bat
@@ -1,5 +1,6 @@
-
+chcp 65001 
 javac -d bin -sourcepath src -cp resources\lib\spring-context-5.1.4.RELEASE.jar;resources\lib\spring-core-5.1.4.RELEASE.jar;resources\lib\spring-beans-5.1.4.RELEASE.jar src\com\springinaction\springidol\MainClass.java src\com\springinaction\springidol\*.java 
+
 
 java -classpath bin;resources\lib\commons-logging-1.2.jar;resources\lib\spring-context-5.1.4.RELEASE.jar;resources\lib\spring-core-5.1.4.RELEASE.jar;resources\lib\spring-beans-5.1.4.RELEASE.jar;resources\lib\spring-expression-5.1.4.RELEASE.jar com.springinaction.springidol.MainClass
 pause
