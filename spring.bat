b7c2e6a27b287b6616b20706218c3068fbb9a7e7
igorkorz <mr.randolph-carter@yandex.ru>
2019-01-16 17:26:13 +0300

    Test

--- spring.bat
+++ spring.bat
@@ -3,3 +3,4 @@ javac -d bin -sourcepath src -cp resources\lib\spring-context-5.1.4.RELEASE.jar;
 
 java -classpath bin;resources\lib\commons-logging-1.2.jar;resources\lib\spring-context-5.1.4.RELEASE.jar;resources\lib\spring-core-5.1.4.RELEASE.jar;resources\lib\spring-beans-5.1.4.RELEASE.jar;resources\lib\spring-expression-5.1.4.RELEASE.jar com.springinaction.springidol.MainClass
 pause
+mkdir fu
