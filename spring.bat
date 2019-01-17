11eab1a53a7bdf2d72dbed76f2cf01522c26c4b4
igorkorz <mr.randolph-carter@yandex.ru>
2019-01-17 11:48:30 +0300

    Test

--- spring.bat
+++ spring.bat
@@ -9,3 +9,4 @@ mkdir fu
 =======
 java -classpath bin;resources\lib\commons-logging-1.2.jar;resources\lib\spring-context-5.1.4.RELEASE.jar;resources\lib\spring-core-5.1.4.RELEASE.jar;resources\lib\spring-beans-5.1.4.RELEASE.jar;resources\lib\spring-expression-5.1.4.RELEASE.jar com.springinaction.springidol.MainClass
 >>>>>>> 85932cfc6053d666d39362e75a8bfa725421a4af
+-
